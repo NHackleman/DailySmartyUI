b26c66857fc38bb82b4b72d6d88e2d84541350b9
Nathan <hacklemannathan@gmail.com>
2022-08-24 22:28:39 -0500

    Reduced project to blank page

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>Logo</div>
+			</div>
+		);
+	}
 }
